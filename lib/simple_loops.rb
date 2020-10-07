dc2b9634e22aecfe8758a83da47d8fb9aa84bcce
David Medina <davidamedina619@gmail.com>
2020-10-07 19:50:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,11 @@
 # Write your methods here
 def loop_message_five_times(message)
- 5.times do puts message
+ 5.times do 
+   puts message
  end 
+end
+
+
+def loop_message_n_times(message,number)
+  
 end 
